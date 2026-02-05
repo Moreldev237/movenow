9147b1022dc86dc742bc587e2418514e9d561d7a
Moreldev237 <Moreldev237@gmail.com>
2026-02-05 17:32:33 +0100

    movenow--->mvp:v3

--- accounts/models.py
+++ accounts/models.py
@@ -1,10 +1,41 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
 from django.contrib.gis.db import models as gis_models
 from django.contrib.gis.geos import Point
 import uuid
+
+class UserManager(BaseUserManager):
+    """Custom manager for User model with email as username field."""
+
+    def _create_user(self, email, password=None, **extra_fields):
+        """Create and save a user with the given email and password."""
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular user with the given email and password."""
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create and save a superuser with the given email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
     """Modèle utilisateur personnalisé"""
@@ -101,7 +132,9 @@ class User(AbstractUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['phone', 'first_name', 'last_name']
-    
+
+    objects = UserManager()
+
     class Meta:
         verbose_name = _("Utilisateur")
         verbose_name_plural = _("Utilisateurs")
